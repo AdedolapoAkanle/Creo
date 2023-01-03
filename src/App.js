eb0a7b52a54adf26700cf9fd5f53546fab7afee0
AdedolapoAkanle <dolapoakanle@gmail.com>
2023-01-03 14:08:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  // const [theme, setTheme] = useState("light");
+  // const toggleTheme = () => {
+  //   if (theme === "light") {
+  //     setTheme("dark");
+  //   } else {
+  //     setTheme("light");
+  //   }
+  // };
+  // useEffect(() => {
+  //   document.body.className = theme;
+  // }, [theme]);
+  //`App ${theme}`
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button onClick={toggleTheme}>Switch</button> */}
+      <div className="container">
+        <section className="section">
+          <div className="blur">
+            <div className="section-nav">
+              <div className="name">Creo</div>
+              <li className="list">
+                <ul>Home</ul>
+                <ul>About</ul>
+                <ul>Pricing</ul>
+                <ul>Contact Us</ul>
+              </li>
+            </div>
+            <div className="main">
+              <div className="text-top">Bring out the creativity</div>
+              <div className="text-bottom">
+                We are idea-driven, working with a strong focus on design and
+                user experience. That is what your brand and audience deserve
+              </div>
+              <div className="button">get started</div>
+            </div>
+          </div>
+        </section>
+        <section className="about-section">
+          <div className="left">
+            <div className="about">about us</div>
+            <div className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+              facere. Eos ut, in temporibus magni perspiciatis expedita unde
+              eligendi tenetur quaerat quasi animi iure totam et doloribus fuga
+              consequuntur fugit.
+            </div>
+
+            <li>
+              <ul>
+                <img src="./img/Vector(1).png" alt="" />
+                Design
+              </ul>
+              <ul>
+                <img src="img/" alt="" />
+                Branding
+              </ul>
+            </li>
+          </div>
+          <div className="right"></div>
+        </section>
+      </div>
     </div>
   );
 }
